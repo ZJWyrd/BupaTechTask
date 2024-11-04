1563f303bf4c8501a21cbc392cad263f2642ecdb
ZJWyrd <Zhloe@JayeWyrd.com>
2024-11-04 08:48:52 +0000

    Copy in project

---
+++ Components/Models/VehicleData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZJWyrdBupa.Components.Models;
+
+public class VehicleData
+{
+    [RegularExpression(@"^(?=.{1,7})(([a-zA-Z]?){1,3}(\d){1,3}([a-zA-Z]?){1,3})$",
+        ErrorMessage = "Invalid Input, have you removed spaces?")]
+    public string Registration { get; set; }
+    public string Make { get;set; }
+    public string Model { get;set; }
+    public string Colour { get;set; }
+    public DateTime MOTExpiry { get;set; }
+    public int LastMileage { get;set; }
+}
